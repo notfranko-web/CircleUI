698309ec3e40c606ff6c8f6e8b98523cd6e43ec7
notfranko <growfrog777@gmail.com>
2026-03-19 15:11:14 +0200

    Update .gitignore

--- CircleUI.Data/Migrations/20260319114620_InitialMigration.Designer.cs
+++ CircleUI.Data/Migrations/20260319114620_InitialMigration.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CircleUI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260312162445_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20260319114620_InitialMigration")]
+    partial class InitialMigration
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- CircleUI.Data/Migrations/20260319114620_InitialMigration.cs
+++ CircleUI.Data/Migrations/20260319114620_InitialMigration.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace CircleUI.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class InitialMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
