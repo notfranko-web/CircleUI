65b3d2bdd0dc52dda95b81def05b0ac1b5d47dc8
notfranko <growfrog777@gmail.com>
2026-03-25 10:47:01 +0200

    commit forgotten IAssetService class

--- CircleUI.Core/Interfaces/IAssetService.cs
+++ CircleUI.Core/Interfaces/IAssetService.cs
@@ -1,3 +1,4 @@
+using CircleUI.Core.DTOs;
 using CircleUI.Data.Models;
 
 namespace CircleUI.Core.Interfaces;
@@ -5,5 +6,8 @@ namespace CircleUI.Core.Interfaces;
 public interface IAssetService
 {
     public Task<List<Asset>> GetAll();
-    
+    public Task<Asset> Create(AssetDTO assets);
+    public Task<AssetDTO> GetById(string id);
+    public Task<Asset> Update(AssetDTO assets);
+    public Task Delete(string id);
 }
