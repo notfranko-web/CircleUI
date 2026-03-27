9256dbf9cd38f11e02426a5333f62cd8a677a4fb
notfranko <growfrog777@gmail.com>
2026-03-27 15:31:05 +0200

    Create ProjectBuilderViewModel

---
+++ CircleUI/ViewModels/WebsiteProject/WebsiteProjectBuilderViewModel.cs
@@ -0,0 +1,15 @@
+using CircleUI.Core.DTOs;
+
+namespace CircleUI.ViewModels.WebsiteProject;
+
+public class WebsiteProjectBuilderViewModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "New Project";
+    public string Description { get; set; } = string.Empty;
+    public string Domain { get; set; } = string.Empty;
+    public bool IsPublished { get; set; }
+    public string UserId { get; set; } = null!;
+    public List<ComponentDTO> ComponentDtos = new List<ComponentDTO>();
+    public List<PageDTO> PageDtos = new List<PageDTO>();
+}
